a01dbb550360d09a6dcea816a1fe67f37aac711b
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-07-09 16:36:02 +0100

    refactor cliopatria_gdf as a single func

--- seshat/apps/core/management/commands/populate_videodata.py
+++ seshat/apps/core/management/commands/populate_videodata.py
@@ -79,10 +79,14 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS('Simplified geometries added'))
 
 
-def vectorized_convert_name(gdf):
+def cliopatria_gdf(cliopatria_geojson_path):
     """
-    Corrected vectorized version of convert_name to process the entire DataFrame at once.
+    Load the Cliopatria shape dataset with GeoPandas, process names and colors efficiently.
     """
+    gdf = gpd.read_file(cliopatria_geojson_path)
+
+    self.stdout.write(self.style.SUCCESS(f'Generating shape names...'))
+
     # Remove parentheses from 'Name' and 'MemberOf'
     gdf['CleanName'] = gdf['Name'].str.replace('[()]', '', regex=True)
     gdf['CleanMember_of'] = gdf['MemberOf'].str.replace('[()]', '', regex=True)
@@ -91,8 +95,7 @@ def vectorized_convert_name(gdf):
     gdf['DisplayName'] = gdf['CleanName']
     gdf['ColorKey'] = gdf['CleanName']
 
-    # Conditions for setting DisplayName to None
-    # If the shape has components, is not a personal union, and the components don't have components
+    # Conditions for setting DisplayName to None: If the shape has components, is not a personal union, and the components don't have components
     has_components = gdf['Components'].notna() & gdf['Components'].str.len() > 0
     not_personal_union = ~gdf['SeshatID'].str.contains(';')
     components_without_components = ~gdf['Components'].str.contains('\\(')
@@ -104,36 +107,29 @@ def vectorized_convert_name(gdf):
     # Add type prefix to DisplayName where type is not 'POLITY'
     gdf.loc[gdf['Type'] != 'POLITY', 'DisplayName'] = gdf['Type'] + ': ' + gdf['DisplayName']
 
-def cliopatria_gdf(cliopatria_geojson_path):
-    """
-    Load the Cliopatria shape dataset with GeoPandas, process names and colors efficiently.
-    """
-    gdf = gpd.read_file(cliopatria_geojson_path)
-
-    # Apply vectorized name conversion
-    self.stdout.write(self.style.SUCCESS(f'Generating shape names...'))
-    vectorized_convert_name(gdf)
     self.stdout.write(self.style.SUCCESS(f'Generated shape names for {len(gdf)} shapes.'))
+    self.stdout.write(self.style.SUCCESS(f'Assigning colours to shapes...'))
 
     # Use DistinctiPy package to assign a colour based on the ColorKey field
-    self.stdout.write(self.style.SUCCESS(f'Assigning colours to shapes...'))
     colour_keys = gdf['ColorKey'].unique()
     colours = [get_hex(col) for col in get_colors(len(colour_keys))]
     colour_mapping = dict(zip(colour_keys, colours))
 
     # Map colors to a new column efficiently
     gdf['Color'] = gdf['ColorKey'].map(colour_mapping)
-    self.stdout.write(self.style.SUCCESS(f'Assigned colours to {len(gdf)} shapes.'))
 
     # Drop intermediate columns
     gdf.drop(['CleanName', 'CleanMember_of'], axis=1, inplace=True)
 
+    self.stdout.write(self.style.SUCCESS(f'Assigned colours to {len(gdf)} shapes.'))
     self.stdout.write(self.style.SUCCESS(f'Determing polity start and end years...'))
+
     # Add a column called 'PolityStartYear' to the GeoDataFrame which is the minimum 'FromYear' of all shapes with the same 'Name'
     gdf['PolityStartYear'] = gdf.groupby('Name')['FromYear'].transform('min')
 
     # Add a column called 'PolityEndYear' to the GeoDataFrame which is the maximum 'ToYear' of all shapes with the same 'Name'
     gdf['PolityEndYear'] = gdf.groupby('Name')['ToYear'].transform('max')
+
     self.stdout.write(self.style.SUCCESS(f'Determined polity start and end years for {len(gdf)} shapes.'))
 
     return gdf
