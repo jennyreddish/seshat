3b8704be62331a088c7e5a23f32cc86f3b362f12
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-07-10 14:30:03 +0100

    Ignore rows with None DisplayName

--- seshat/apps/core/management/commands/populate_videodata.py
+++ seshat/apps/core/management/commands/populate_videodata.py
@@ -39,6 +39,8 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS('Adding data to the database...'))
         for feature in cliopatria_data['features']:
             properties = feature['properties']
+            if properties['DisplayName'] is None:  # Ignore rows where the DisplayName is None
+                continue
             self.stdout.write(self.style.SUCCESS(f"Creating VideoShapefile instance for {properties['DisplayName']} ({properties['FromYear']} - {properties['ToYear']})"))
             
             # Save geom and convert Polygon to MultiPolygon if necessary
