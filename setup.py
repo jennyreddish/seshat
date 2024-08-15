a9d04725f71eed527ab7d58e8515761f3f51eaaf
Ed Chalstrey <edwardchalstrey@gmail.com>
2024-08-15 13:33:42 +0100

    update authors

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='seshat',
     version='0.1.0',
     description='Seshat: Global History Databank brings together the most current and comprehensive body of knowledge about human history in one place',
-    url='https://github.com/MajidBenam/seshat',
-    author='Majid Benam',
+    url='https://github.com/Seshat-Global-History-Databank/seshat',
+    author='Majid Benam, Ed Chalstrey, Kalle Westerling',
     author_email='benam@csh.ac.at',
     license='',  # TODO
     packages=find_packages(),
